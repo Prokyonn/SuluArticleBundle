60c1745b4cfb7432685f132ae151f0c669331322
Oliver Kossin <thecadiens@gmail.com>
2024-05-16 16:43:41 +0200

    Add single article selection content type (#2)

--- src/Infrastructure/Symfony/HttpKernel/SuluArticleBundle.php
+++ src/Infrastructure/Symfony/HttpKernel/SuluArticleBundle.php
@@ -22,6 +22,7 @@ use Sulu\Article\Infrastructure\Sulu\Content\ArticleLinkProvider;
 use Sulu\Article\Infrastructure\Sulu\Content\ArticleSelectionContentType;
 use Sulu\Article\Infrastructure\Sulu\Content\ArticleSitemapProvider;
 use Sulu\Article\Infrastructure\Sulu\Content\ArticleTeaserProvider;
+use Sulu\Article\Infrastructure\Sulu\Content\SingleArticleSelectionContentType;
 use Sulu\Article\UserInterface\Controller\Admin\ArticleController;
 use Sulu\Bundle\ContentBundle\Content\Infrastructure\Sulu\Preview\ContentObjectProvider;
 use Sulu\Bundle\ContentBundle\Content\Infrastructure\Sulu\Search\ContentSearchMetadataProvider;
@@ -245,6 +246,15 @@ final class SuluArticleBundle extends AbstractBundle
         $services->set('sulu_article.article_reference_store')
             ->class(ReferenceStore::class)
             ->tag('sulu_website.reference_store', ['alias' => ArticleInterface::RESOURCE_KEY]);
+
+        $services->set('sulu_article.content_types.single_article_selection')
+            ->class(SingleArticleSelectionContentType::class)
+            ->args([
+                new Reference('sulu_article.article_repository'),
+                new Reference('sulu_content.content_manager'),
+                new Reference('sulu_article.article_reference_store')
+            ])
+            ->tag('sulu.content.type', ['alias' => 'single_article_selection']);
 
         $services->set('sulu_article.content_types.article_selection')
             ->class(ArticleSelectionContentType::class)
